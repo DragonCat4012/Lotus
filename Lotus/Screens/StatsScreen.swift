209a060457bf410dc49f0d5a656bdf6b3d6d5a16
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-07-27 16:31:56 +0200

    hm

--- Lotus/Screens/StatsScreen.swift
+++ Lotus/Screens/StatsScreen.swift
@@ -23,12 +23,44 @@ struct StatsScreen: View {
                 .opacity(0.6)
             
             VStack {
-                Text("Some Stats \(vm.selectedYear)")
+                Text("Some Stats \(String(vm.selectedYear))")
                     .font(.title)
-                Text("h")
+                
+                barChart().padding(.vertical).background(Color.blue).frame(height: 20)
+                
+                topTypes()
+                
             }.foregroundColor(.white)
            
         }.ignoresSafeArea()
+    }
+    
+    func topTypes() -> some View {
+        VStack {
+            HStack {
+                RoundedRectangle(cornerRadius: 8).fill(Color.orange).frame(width: 50, height: 50)
+                RoundedRectangle(cornerRadius: 8).fill(Color.orange).frame(width: 50, height: 50)
+                RoundedRectangle(cornerRadius: 8).fill(Color.orange).frame(width: 50, height: 50)
+            }
+            
+            HStack {
+                Text("#1").frame(width: 50)
+                Text("#2").frame(width: 50)
+                Text("#3").frame(width: 50)
+            }
+        }
+    }
+    
+    func barChart() -> some View {
+        GeometryReader { geo in
+            let width = geo.size.width * 0.8
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 8).fill(Color.red).frame(width: width).frame(height: 20)
+                RoundedRectangle(cornerRadius: 8).fill(Color.orange).frame(width: width * 0.4).frame(height: 20)
+                RoundedRectangle(cornerRadius: 8).fill(Color.green).frame(width: width * 0.2).frame(height: 20)
+            }
+        }
+        
     }
 }
 
